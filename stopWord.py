0546ea107492493901e32ee0d8ff997593675991
Hari Govind <grhari1998@gmail.com>
2018-01-28 04:22:45 -0500

    added a python script that deletes the useless words in the collected data

---
+++ stopWord.py
@@ -0,0 +1,12 @@
+import io
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+stop_words = set(stopwords.words('english'))
+file = open("test.txt")
+line = file.read()# Use this to read file content as a stream:
+words = line.split()
+for s in words:
+    if not (s in stop_words):
+        appendFile = open('filteredtext.txt', 'a')
+        appendFile.write(s + " ")
+        appendFile.close()
