af4b37928bd7d11ce649a254ca0b8dd6d68a6e1b
Hari Govind <grhari1998@gmail.com>
2018-01-28 05:17:09 -0500

    tidied up smelly code in stopWord.py'

--- stopWord.py
+++ stopWord.py
@@ -1,12 +1,22 @@
 import io
+import json
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-stop_words = set(stopwords.words('english'))
-file = open("data.txt")
-line = file.read()# Use this to read file content as a stream:
-words = line.split()
-for s in words:
-    if not (s in stop_words):
-        appendFile = open('filteredtext.txt', 'a')
-        appendFile.write(s + " ")
-        appendFile.close()
+
+def main():
+	stop_words = set(stopwords.words('english'))
+	file = open('data.txt')
+	line = file.read()
+	words = line.split()
+	for s in words:
+		if not s in stop_words:
+			appendFile=open('filteredtext.txt','a')
+			appendFile.write(s + " ")
+			appendFile.close()
+	file.close()
+	file2=open('filteredtext.txt','r').read()
+	jsonObj = json.loads(file2)
+	print(jsonObj)
+
+if __name__ ==  "__main__":
+	main()
